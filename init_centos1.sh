555238274b2525f6af307eef24b09844f074ace8
Steve Feldman <feldman@twincreeks.net>
2021-06-12 22:17:19 +0000

    add files

---
+++ init_centos1.sh
@@ -0,0 +1,6 @@
+/usr/bin/sleep 2
+/usr/sbin/ip address add 10.100.1.2/24 dev eth1
+/usr/sbin/ip address add 2001:db8::2/64 dev eth1
+/usr/sbin/ip route add 10.0.0.0/8 via 10.100.1.1 dev eth1
+/usr/sbin/ip route add 2001:db8::/32 via 2001:db8::1 dev eth1
+/bin/bash
